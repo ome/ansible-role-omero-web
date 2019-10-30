2fbcd9cd8f518510a5e75cb2bccbc5ebe342cfa2
Simon Li <spli@dundee.ac.uk>
2019-10-30 17:20:15 +0000

    molecule2: fix tests

--- molecule/active/tests/test_default.py
+++ molecule/active/tests/test_default.py
@@ -12,16 +12,16 @@ OMERO = '/opt/omero/web/OMERO.web/bin/omero'
 VERSION_PATTERN = re.compile('(\d+)\.(\d+)\.(\d+)-ice36-')
 
 
-def test_omero_web_config(Command, Sudo):
-    with Sudo('omero-web'):
-        cfg = Command.check_output("%s config get" % OMERO)
+def test_omero_web_config(host):
+    with host.sudo('omero-web'):
+        cfg = host.check_output("%s config get" % OMERO)
     assert cfg == (
         'omero.web.server_list=[["localhost", 12345, "molecule-test"]]')
 
 
-def test_omero_version(Command, Sudo):
-    with Sudo('omero-web'):
-        ver = Command.check_output("%s version" % OMERO)
+def test_omero_version(host):
+    with host.sudo('omero-web'):
+        ver = host.check_output("%s version" % OMERO)
     m = VERSION_PATTERN.match(ver)
     assert m is not None
     assert int(m.group(1)) >= 5
@@ -29,17 +29,17 @@ def test_omero_version(Command, Sudo):
 
 
 @pytest.mark.parametrize("name", ["omero-web", "nginx"])
-def test_services_running_and_enabled(Service, name):
-    service = Service(name)
+def test_services_running_and_enabled(host, name):
+    service = host.service(name)
     assert service.is_running
     assert service.is_enabled
 
 
-def test_nginx_gateway(Command):
-    out = Command.check_output('curl -L localhost')
+def test_nginx_gateway(host):
+    out = host.check_output('curl -L localhost')
     assert 'OMERO.web - Login' in out
 
 
-def test_omero_web_config_applied(Command, Sudo):
-    out = Command.check_output('curl -L localhost')
+def test_omero_web_config_applied(host):
+    out = host.check_output('curl -L localhost')
     assert 'molecule-test:12345' in out
--- molecule/inactive/tests/test_default.py
+++ molecule/inactive/tests/test_default.py
@@ -9,26 +9,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 OMERO = '/opt/omero/web/OMERO.web/bin/omero'
 
 
-def test_omero_web_config(Command, Sudo):
-    with Sudo('omero-web'):
-        cfg = Command.check_output("%s config get" % OMERO)
+def test_omero_web_config(host):
+    with host.sudo('omero-web'):
+        cfg = host.check_output("%s config get" % OMERO)
     assert cfg == (
         'omero.web.server_list=[["localhost", 12345, "molecule-test"]]')
 
 
-def test_omero_version(Command, Sudo):
-    with Sudo('omero-web'):
-        ver = Command.check_output("%s version" % OMERO)
+def test_omero_version(host):
+    with host.sudo('omero-web'):
+        ver = host.check_output("%s version" % OMERO)
     assert ver.startswith('5.3.')
 
 
-def test_nginx_not_configured(File):
-    assert not File('/etc/nginx/conf.d/omero-web.conf').exists
+def test_nginx_not_configured(host):
+    assert not host.file('/etc/nginx/conf.d/omero-web.conf').exists
 
 
-def test_nginx_not_installed(Package):
-    assert not Package('nginx').is_installed
+def test_nginx_not_installed(host):
+    assert not host.package('nginx').is_installed
 
 
-def test_omeroweb_systemd_configured(File):
-    assert File('/etc/systemd/system/omero-web.service').exists
+def test_omeroweb_systemd_configured(host):
+    assert host.file('/etc/systemd/system/omero-web.service').exists
